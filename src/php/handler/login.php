0ad3c3dc7082dd2938bae38b9ab73bf8dcf58002
Nataniel <nataniel.haraldseth@hotmail.com>
2019-04-10 12:27:36 +0200

    Add handler for login

--- src/php/handler/login.php
+++ src/php/handler/login.php
@@ -1,1 +1,20 @@
-<?php // Nate; legg til handleren din her.
+<?php 
+session_start();
+include_once '../class/User.php';
+include_once '../class/RelativeRoot.php';
+$user = new User();
+
+if (isset($_POST['submit'])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $login = $user->check_login($email, $password);
+    if ($login) {
+        //Login Success
+        header("location:../../../index.php?p=main");
+    }else {
+        //Login failed
+        header("location:../../../index.php?p=login");
+    }
+}
+?>
