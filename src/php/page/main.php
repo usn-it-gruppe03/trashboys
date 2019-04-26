0dc2cd42e5d6300867a9f7dd3be2627c2a5e8263
Nataniel <nataniel.haraldseth@hotmail.com>
2019-04-27 01:30:46 +0200

    Check for user details
    If a user deletes a session manually this will repopulate the session data.

--- src/php/page/main.php
+++ src/php/page/main.php
@@ -13,6 +13,7 @@
     $house_letter   = User::get_house_letter();
     $zip_code       = User::get_zip_code();
     $postal_location= User::get_postal_location();
+    $user->check_user();
 
 ?>
 <!-- Page: Home -->
